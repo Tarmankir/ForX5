a84ac3a21250b006fad1bb314e7ff23b9ce6b35e
aleksey_krapivnitskiy <krapivnitskiy@secureducation.ru>
2021-02-22 17:24:09 +0300

    added todo

--- src/main/java/pages/SelenideUse.java
+++ src/main/java/pages/SelenideUse.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideUse {
 
-    //todo why test in main/java?
+    //todo why test in main/java? move to src/test
     @Test
     void selenideUse() {
         open("https://forum.awd.ru/");
